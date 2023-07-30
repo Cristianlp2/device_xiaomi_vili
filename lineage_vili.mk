dc7c5b5dd2899d70c3d66993de9a8b3306038631
Cristianlp2 <cristianlpu182@gmail.com>
2023-07-29 23:54:00 -0400

    vili: Alpha Bring Up

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -11,23 +11,35 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vili device
 $(call inherit-product, device/xiaomi/vili/device.mk)
 
-# Inherit some common EvolutionX stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Boot Animation
-TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# AlphaDroid
+ALPHA_BUILD_TYPE := UNOFFICIAL
+ALPHA_MAINTAINER := Cristianlp
+
+# Gapps Flags
+WITH_GAPPS := 2
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_USE_PIXEL_FRAMEWORK := true
+
+# Debugging Flags
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := false
 
 # MIUI stuff
 TARGET_SHIPS_GALLERY=true
-
-# Rom additions
-TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := vili
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2107113SG
-PRODUCT_NAME := evolution_vili
+PRODUCT_NAME := lineage_vili
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vili-user 13 RKQ1.211001.001 V14.0.3.0.TKDMIXM release-keys"
